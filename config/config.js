85b0253b5d1a6d1a4cfa120cba73a5eb745ede24
Ramiro Berrelleza <rberrelleza@gmail.com>
2016-06-15 12:28:03 -0700

    update config.js to have a default resolution

--- config/config.js
+++ config/config.js
@@ -6,7 +6,15 @@ var config = {
     },
     useNicks: false,
     bosh: '//jitsi.example.com/http-bind', // FIXME: use xep-0156 for that
-    desktopSharing: 'false' // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
+    desktopSharing: 'ext' // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
+    minHDHeight: 540,
+    startBitrate: "800",
+    resolution: 1080,
+    useRtcpMux: true,
+    useBundle: true,
+    enableLipSync: true,
+    openSctp: true, // Toggle to enable/disable SCTP channels
+    channelLastN: -1, // The default value of the channel attribute last-n.
     //chromeExtensionId: 'diibjkoicjeejcmhdnailmkgecihlobk', // Id of desktop streamer Chrome extension
     //minChromeExtVersion: '0.1' // Required version of Chrome extension
 };
